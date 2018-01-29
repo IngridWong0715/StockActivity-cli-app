9089719b214849f1b2938ff6e0f0e198a17ae6c6
ingridwong0715 <ingrid830715@gmail.com>
2018-01-29 15:56:28 -0500

    Completed StockActivity::Scraper.scrape_most_active

--- lib/stock_activity/scraper.rb
+++ lib/stock_activity/scraper.rb
@@ -8,13 +8,6 @@ class StockActivity::Scraper
   def self.scrape_most_active
     doc = Nokogiri::HTML(open("http://www.nasdaq.com/"))
 
-
-    # companies = doc.search("div#Mostactive div.coName a").children #all companies: company name then company_symbol
-    # names = companies.collect do |company|
-    #   company.text
-    # end
-
-
     companies = doc.search("div#Mostactive tbody tr")
 
     all = doc.css("div#Mostactive tr > td").children
@@ -31,12 +24,11 @@ class StockActivity::Scraper
     #turn acc into nested array:
     counter = 0
     subarray = []
-
     nested = acc.reduce([]) do |accumulator, info|
       if counter < 3
         subarray << info
         counter += 1
-      #  binding.pry
+
     elsif counter == 3
         subarray << info
         accumulator << subarray
@@ -44,42 +36,36 @@ class StockActivity::Scraper
         counter = 0
 
       end
-    #  binding.pry
 
       accumulator
     end
 
-binding.pry
+
+    companies_info = []
+    nested.each do |company_group|
+
+      company = {}
+      company_group.each_with_index do |info, index|
+
+        if index % 4 == 0
+          company[:company_name] = info
+
+        elsif index % 4 == 1
+
+          company[:last_sale] = info
+        elsif index % 4 == 2
+          company[:change_net_percentage] = info
+        elsif index % 4 == 3
+          company[:share_volume] = info
+          companies_info << company
+        end
+
+      end
+    end
+
+    companies_info
 
 
-
-
-    # companies_info = []
-    # acc.each_with_index do |info, index|
-    #   company = {}
-    #   if index % 4 == 0
-    #     company[:company_name] = info
-    #
-    #   elsif index % 4 == 1
-    #
-    #     company[:last_sale] = info
-    #   elsif index % 4 == 2
-    #     company[:change_net_percentage] = info
-    #   elsif index % 4 == 3
-    #     company[:share_volume] = info
-    #     companies_info << company
-    #   end
-    #
-    # end
-    # binding.pry
-    # companies_info
-
-
-    #return : ["Apple Inc. (AAPL)", "$168.7602", "2.75 ▼ 1.60%", "30,994,884", "Intel Corporation (INTC)", "$50.37", "0.29 ▲ 0.58%", "24,601,812", "PowerShares QQQ Trust, Series 1 (QQQ)", "$170.705", "0.23 ▼ 0.13%", "21,308,460", "Micron Technology, Inc. (MU)", "$42.97", "0.70 ▼ 1.60%", "18,873,253", "Wynn Resorts, Limited (WYNN)", "$164.65", "15.64 ▼ 8.67%", "18,731,482"]
-
-
-    #returns an array of hashes
-    #each hash represent a company {:company => , :last_sale, :change_net_percentage, :share_volume}
   end
 
   def self.scrape_most_advanced
