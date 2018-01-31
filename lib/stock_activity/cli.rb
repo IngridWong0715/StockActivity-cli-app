7bc79bb02a3b2ad736c2cd680daf86c69628e106
ingridwong0715 <ingrid830715@gmail.com>
2018-01-31 12:11:33 -0500

    Implemented display_stock_details

--- lib/stock_activity/cli.rb
+++ lib/stock_activity/cli.rb
@@ -1,6 +1,8 @@
 
 
 class StockActivity::CLI
+
+  attr_accessor :second_input
   def call
     puts "Welcome to Stock Activities! Which kind of stock activity would you like to see?"
     start
@@ -18,17 +20,10 @@ class StockActivity::CLI
     end
     display_stocks
 
-    searched_stock = StockActivity::Stock.find_by_name("Apple ")
-    attributes_hash = StockActivity::Scraper.scrape_stock_details(searched_stock.url)
-    searched_stock.add_more_attributes(attributes_hash)
-    
-
-binding.pry
-
     ### end first time
 
     puts "Would you like to get more details on a specific stock, please enter the company symbol. View another category? Or exit?"
-    #second_input = gets.strip.downcase
+    @second_input = gets.strip
 
     ### IMPLEMENT: SPECIFIC Stock
     # 1. find that stock from StockActivity::Stock.all
@@ -37,10 +32,11 @@ binding.pry
     # 4. display the stock
     #
 
+    searched_stock = StockActivity::Stock.find_by_name(second_input)
+    attributes_hash = StockActivity::Scraper.scrape_stock_details(searched_stock.url)
+    searched_stock.add_more_attributes(attributes_hash)
 
-    #
-
-
+    display_stock_details(searched_stock)
 
 
   end
@@ -71,6 +67,29 @@ binding.pry
       puts "\n"
     end
     nil
+  end
+
+  def display_stock_details(stock)
+    puts "\n#{stock.company_name} Common Stock Quote & Summary Data".colorize(:blue)
+    puts "#{stock.last_sale.colorize(:magenta)} #{stock.change_net_percentage.colorize(:magenta)}"
+    puts "\n\n"
+    puts "Key Stock Data \n"
+    puts "Best Bid / Ask: " + stock.best_bid_ask.colorize(:magenta)
+    puts "1 Year Target: " + stock.year_target.colorize(:magenta)
+    puts "Today's High / Low: " + stock.todays_high_low.colorize(:magenta)
+    puts "Share Volume: " + stock.share_volume.colorize(:magenta)
+    puts "50 Day Avg. Daily Volume: " + stock.day_avg_daily_volume.colorize(:magenta)
+    puts "Previous Close: " + stock.previous_close.colorize(:magenta)
+    puts "52 Week High / Low: " + stock.week_high_low.colorize(:magenta)
+    puts "Market Cap: " + stock.market_cap.colorize(:magenta)
+    puts "P/E Ratio: " + stock.pe_ratio.colorize(:magenta)
+    puts "Forward P/E (1y): " + stock.forward_pe_1y.colorize(:blue)
+    puts "Earnings Per Share (EPS): " + stock.earnings_per_share_eps.colorize(:magenta)
+    puts "Annualized Dividend: " + stock.annualized_dividend.colorize(:magenta)
+    puts "Ex Dividend Date: " + stock.ex_dividend_date.colorize(:magenta)
+    puts "Dividend Payment Date: " + stock.dividend_payment_date.colorize(:magenta)
+    puts "Current Yield: " + stock.current_yield.colorize(:magenta)
+    puts "Beta: " + stock.beta.colorize(:magenta)
   end
 
 
